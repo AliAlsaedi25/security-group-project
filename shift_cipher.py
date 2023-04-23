7b214a251be15d251405112135e0c1d874eedc3b
gzpcho <roquebertian@gmail.com>
2023-04-23 02:05:00 -0500

    refactor caesar cipher script

--- shift_cipher.py
+++ shift_cipher.py
@@ -1,44 +1,47 @@
 import string
 
-fileToOpen = 'new.txt' #input("What file would you like to open: ")
-fileToWrite = 'hope.txt' #input("What file would you like to write to: ")
+FILE_TO_OPEN = 'new.txt'
+FILE_TO_WRITE = 'hope.txt'
+ALPHABET_LOWER_BOUND = 0
+ALPHABET_UPPER_BOUND = ord('~') + 1
 
-inputFile = open(fileToOpen, "r")
-plain_text = inputFile.read()
-altered_text = open(fileToWrite, "w")
+def encryptCaesarCipher(plain_text, shift = 2):
+    # Remove redundant shift amount
+    shift = shift % ALPHABET_UPPER_BOUND
 
+    # Creating an alphabet for all ASCII characters
+    alphabet = ''.join(chr(i) for i in range(ALPHABET_UPPER_BOUND))
 
-operation = input("Encrypt or Decrypt [e] or [d]: ")
-shift_val = int(input("Input Shift Value: "))
-
-
-
-
-def CC(text, shift = 2):
-
-    shift = shift % 128
-
-    #creating an alphabet for all ascii characters
-    alphabet = ''.join(chr(i) for i in range(128))
-    
+    # Create a shifted version of that alphabet
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+
+    # Create a dictionary where keys are ASCII values and the values are shifted ASCII values
     table = str.maketrans(alphabet, shifted_alphabet)
-    altered = text.translate(table)
 
-    altered_text.write(altered)
-
-
-
-def DCC(text, shift = 2):
-    shift = (shift * -1) % 128
-    CC(text, shift)
+    # Use table to map each character in the plaintext to its shifted correspondence
+    return plain_text.translate(table)
 
 
-if operation == 'e':
-    CC(plain_text, shift_val)
-elif operation == 'd':
-    DCC(plain_text, shift_val)
+def decryptCaesarCipher(cipher_text, shift = 2):
+    # Encrypt with inverted direction
+    return encryptCaesarCipher(cipher_text, -shift)
 
 
+if __name__ == "__main__":
+    operation = input("Encrypt or Decrypt [e] or [d]: ")
+    shift = int(input("Input Shift Value: "))
 
-inputFile.close()
+    with open(FILE_TO_OPEN, "r") as f:
+        print(f"Reading from {FILE_TO_OPEN}")
+        input_text = f.read()
+
+        if operation == 'e':
+            result = encryptCaesarCipher(input_text, shift)
+        elif operation == 'd':
+            result = decryptCaesarCipher(input_text, shift)
+
+        print(f"Writing to {FILE_TO_WRITE}")
+        output_file = open(FILE_TO_WRITE, "w")
+        output_file.write(result)
+        output_file.close()
+        f.close()
